b970bd0e710fc4b29eeabcaa8518b18c68916bbd
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2017-03-13 23:45:16 -0300

    Updated

--- src/gameObjects/weapons/StylesBullet.ts
+++ src/gameObjects/weapons/StylesBullet.ts
@@ -4,6 +4,7 @@ module $MultiSpaceship$.Client {
         static toObject(game:Phaser.Game, sender: Phaser.Sprite, destination: Phaser.Sprite, speed?: number){
             return function(bullet: Phaser.Sprite){
                 bullet.reset(sender.x, sender.y);
+                bullet.rotation = game.physics.arcade.angleBetween(sender, destination);
                 game.physics.arcade.moveToObject(bullet, destination, speed);
             };
         }
@@ -11,6 +12,7 @@ module $MultiSpaceship$.Client {
         static forward(sender: Phaser.Sprite, speed: number){
             return function(bullet: Phaser.Sprite){
                 bullet.reset(sender.x, sender.y);
+                bullet.angle  = sender.angle;
                 this.game.physics.arcade.velocityFromAngle(sender.angle, speed, bullet.body.velocity);
             }
         }
--- src/gameStates/level01.ts
+++ src/gameStates/level01.ts
@@ -12,29 +12,42 @@
             this.physics.startSystem(Phaser.Physics.ARCADE);
             this.world.setBounds(0, 0, 2000, this.game.height);
 
+            //background
             this.background = this.game.add.tileSprite(0, 0, this.world.bounds.width, this.world.bounds.height, 'starfield');
-
             var imageBg = this.game.cache.getImage(this.background.key.toString());
-            this.background.tileScale.x = this.world.width / imageBg.width;
-            this.background.tileScale.y = this.world.height / imageBg.height;
+            this.background.tileScale.x = this.game.width / imageBg.width;
+            this.background.tileScale.y = this.game.height / imageBg.height;
             this.background.autoScroll(-80, 0);
 
+            //player
             this.mySpaceship = new Spaceship(this.game, this.world.centerX, this.world.centerY);
             this.mySpaceship.weapon = new LaserWeapon(this.game, this.mySpaceship);
             this.mySpaceship.weapon.styleWeapon = StylesBullet.forward(this.mySpaceship, this.mySpaceship.weapon.getObjectPhaser().bulletSpeed);
-
             this.game.camera.follow(this.mySpaceship);
 
-            this.groupSpaceship02 = this.game.add.group();
-            for (var i = 0; i < 10; i++) {
-                let invader = new Spaceship02(this.game, this.world.randomX, this.world.randomY);
-                invader.weapon = new BallWeapon(this.game, this.mySpaceship);
-                invader.weapon.styleWeapon = StylesBullet.toObject(this.game, invader, this.mySpaceship, invader.weapon.getObjectPhaser().bulletSpeed);
-
-                this.groupSpaceship02.add(invader);
-            }
+            this.generateEnemy();
 
             this.game.debug.text("Use Right and Left arrow keys to move the bat", 0, this.world.height, "red");
+        }
+
+        render(){
+            //this.game.debug.spriteBounds(this.mySpaceship);
+            // this.game.debug.spriteCoords(this.mySpaceship, 20, 32);
+            // this.mySpaceship.weapon.getObjectPhaser().debug(400, 32);
+            //game.debug.spriteCorners(sprite, true, true);
+            this.game.debug.text(this.mySpaceship.cameraOffset.x + ' '+ this.mySpaceship.cameraOffset.y, 500, 20);
+            this.game.debug.text(JSON.stringify(this.game.camera.atLimit), 20, 20);
+        }
+
+        update(){
+            this.game.physics.arcade.overlap(this.mySpaceship, this.groupSpaceship02, this.collisionOfSpaceships, null, this);
+            this.game.physics.arcade.overlap(this.groupSpaceship02, this.mySpaceship.weapon.getObjectPhaser().bullets, this.collisionOfSpaceshipAndBullet, null, this);
+
+            this.groupSpaceship02.forEach((child: IShip)=>{
+                this.game.physics.arcade.overlap(this.mySpaceship, child.weapon.getObjectPhaser().bullets, this.collisionOfSpaceshipAndBullet, null, this);
+            }, this);
+
+            //this.groupSpaceship02.getFirstAlive(false).fire();
         }
 
         collisionOfSpaceships(spaceship1: IShip, spaceship2: IShip) {
@@ -47,22 +60,16 @@
             bullet.kill();
         }
 
-        update(){
-            this.game.physics.arcade.overlap(this.mySpaceship, this.groupSpaceship02, this.collisionOfSpaceships, null, this);
-            this.game.physics.arcade.overlap(this.groupSpaceship02, this.mySpaceship.weapon.getObjectPhaser().bullets, this.collisionOfSpaceshipAndBullet, null, this);
+        generateEnemy(){
+            this.groupSpaceship02 = this.game.add.group();
+            for (var i = 0; i < 100; i++) {
+                let invader = new Spaceship02(this.game, this.world.randomX, this.world.randomY);
+                invader.weapon = new LaserWeapon(this.game, invader);
+                invader.weapon.styleWeapon = StylesBullet.toObject(this.game, invader, this.mySpaceship, invader.weapon.getObjectPhaser().bulletSpeed);
 
-            this.groupSpaceship02.forEach((child: IShip)=>{
-                this.game.physics.arcade.overlap(this.mySpaceship, child.weapon.getObjectPhaser().bullets, this.collisionOfSpaceshipAndBullet, null, this);
-            }, this);
+                this.groupSpaceship02.add(invader);
+            }
         }
-
-        render(){
-            //this.game.debug.spriteBounds(this.mySpaceship);
-            // this.game.debug.spriteCoords(this.mySpaceship, 20, 32);
-            // this.mySpaceship.weapon.getObjectPhaser().debug(400, 32);
-            //game.debug.spriteCorners(sprite, true, true);
-        }
-
     }
 
 }
