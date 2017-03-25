24d8f886d3bac29515e6d0386dc523746d248462
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2017-03-25 01:07:17 -0300

    add speed and fire rate in PlasmaWeapon

--- src/gameObjects/weapons/PlasmaWeapon.ts
+++ src/gameObjects/weapons/PlasmaWeapon.ts
@@ -5,6 +5,8 @@ module MultiSpaceship {
         constructor(game: Phaser.Game, parent: Phaser.PluginManager){
             super(game);
             this.bulletClass = PlasmaBullet;
+            this.bulletSpeed = 1200;
+            this.fireRate = 150;
         }
 
         init(owner: Phaser.Sprite){
@@ -16,7 +18,7 @@ module MultiSpaceship {
     class PlasmaBullet extends Bullet {
         constructor(game: Phaser.Game, x: number, y: number, key?: any, frame?: any){
             super(game, x, y, key, frame);
-            this.damageBullet = 500;
+            this.damageBullet = 50;
         }
     }
 }
